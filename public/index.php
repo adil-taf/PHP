b0488194732a0e6304d7b1eb00535ad2d4f4d33a
Adil Tafsast <adil.tafs@gmail.com>
2023-12-16 11:53:50 +0100

    Add a simple router

--- public/index.php
+++ public/index.php
@@ -1,6 +1,22 @@
 <?php
-declare(strict_types = 1);
 
-require __DIR__.'/../vendor/autoload.php';
+declare(strict_types=1);
 
-phpinfo();
+require __DIR__ . '/../vendor/autoload.php';
+
+$router = new App\Router();
+
+$router->register(
+    '/',
+    function () {
+        echo 'Home';
+    }
+);
+$router->register(
+    '/invoices',
+    function () {
+        echo 'Invoices';
+    }
+);
+
+echo $router->resolve($_SERVER['REQUEST_URI']);

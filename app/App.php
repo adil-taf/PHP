f08e2aeb81e63c06374891218179a6f53270017d
Adil Tafsast <adil.tafs@gmail.com>
2023-12-20 15:41:40 +0100

    Add 'App' the entry class

--- app/App.php
+++ app/App.php
@@ -1,1 +1,41 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Exception\RouteNotFoundException;
+
+class App
+{
+    private static \PDO $db;
+
+    public function __construct(protected Router $router, protected array $request, protected array $config)
+    {
+        try {
+            static::$db = new \PDO(
+                $config['driver'] . ':host=' . $config['host'] . ';dbname=' . $config['database'],
+                $config['user'],
+                $config['pass']
+            );
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+    }
+
+    public static function db(): \PDO
+    {
+        return static::$db;
+    }
+
+    public function run()
+    {
+        try {
+            echo $this->router->resolve($this->request['uri'], strtolower($this->request['method']));
+        } catch (RouteNotFoundException) {
+            http_response_code(404);
+
+            echo View::make('error/404');
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use App\App;
+use App\Controllers\HomeController;
+use App\Controllers\InvoiceController;
+use App\Router;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
@@ -10,19 +15,25 @@ $dotenv->load();
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
 
-try {
-    $router = new App\Router();
 
-    $router
-    ->get('/', [App\Controllers\HomeController::class, 'index'])
-    ->get('/download', [App\Controllers\HomeController::class, 'download'])
-    ->post('/upload', [App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
+$router = new Router();
 
-    echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
-} catch (App\Exceptions\RouteNotFoundException $e) {
-    http_response_code(404);
-    echo \App\View::make('error/404');
-}
+$router
+    ->get('/', [HomeController::class, 'index'])
+    ->get('/download', [HomeController::class, 'download'])
+    ->post('/upload', [HomeController::class, 'upload'])
+    ->get('/invoices', [InvoiceController::class, 'index'])
+    ->get('/invoices/create', [InvoiceController::class, 'create'])
+    ->post('/invoices/create', [InvoiceController::class, 'store']);
+
+(new App(
+    $router,
+    ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
+    [
+    'host' => $_ENV['DB_HOST'],
+    'user' => $_ENV['DB_USER'],
+    'pass' => $_ENV['DB_PASS'],
+    'database' => $_ENV['DB_DATABASE'],
+    'driver' => $_ENV['DB_DRIVER'] ?? 'mysql'
+    ]
+))->run();

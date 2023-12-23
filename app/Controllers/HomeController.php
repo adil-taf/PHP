469230bc9b5b12c20334be98e7a15f7687a3fd82
Adil Tafsast <adil.tafs@gmail.com>
2023-12-23 22:57:12 +0100

    Add a basic Dependency Injection Container

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\InvoiceService;
+use App\App;
 use App\View;
 
 class HomeController
 {
     public function index(): View
     {
+        App::$container->get(InvoiceService::class)->process([], 25);
         return View::make('index');
     }
 }

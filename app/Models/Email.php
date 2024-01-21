a16d2b82c7c2cbaa31a4b15bcd2b4349fde7b2de
Adil Tafsast <adil.tafs@gmail.com>
2024-01-21 01:05:24 +0100

    Queue emails

---
+++ app/Models/Email.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Enums\EmailStatus;
+use App\Model;
+use PDO;
+use Symfony\Component\Mime\Address;
+
+class Email extends Model
+{
+    public function queue(
+        Address $to,
+        Address $from,
+        string $subject,
+        string $html,
+        ?string $text = null
+    ): void {
+        $stmt = $this->db->prepare(
+            'INSERT INTO emails (subject, status, html_body, text_body, meta, created_at)
+             VALUES (?, ?, ?, ?, ?, NOW())'
+        );
+
+        $meta['to']   = $to->toString();
+        $meta['from'] = $from->toString();
+
+        $stmt->execute([$subject, EmailStatus::Queue->value, $html, $text, json_encode($meta)]);
+    }
+}
